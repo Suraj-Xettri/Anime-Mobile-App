e2d579d8cde3980113b848336dd9db8248496a0c
Suraj Thapa <surajthapart@gmail.com>
2024-08-28 12:42:57 +0545

    container side scroll view created

--- app/home/homeBody.jsx
+++ app/home/homeBody.jsx
@@ -1,17 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from "react-native";
+import React from "react";
 
 const HomeBody = () => {
+    const data = [
+        {
+          title: "Chainsaw Man",
+          image: require("../../assets/images/one.jpg"),
+          genre: "Action, Comedy, Dark Fantasy",
+        },
+        {
+          title: "Attack on Titan",
+          image: require("../../assets/images/two.jpg"),
+          genre: "Action, Fantasy, Drama",
+        },
+        {
+          title: "My Hero Academia",
+          image: require("../../assets/images/three.jpg"),
+          genre: "Action, Superhero, Adventure",
+        },
+      ];
   return (
     <View className="w-full mt-8">
       <View className="px-4 flex justify-evenly flex-row w-full">
-        <Text className='bg-[#E84A28] text-white px-4 py-1 rounded-xl'>Anime</Text>
-        <Text className='px-4 py-1'>Manhwa</Text>
-        <Text className='px-4 py-1'>Manga</Text>
-        <Text className='px-4 py-1'>Manhwa</Text>
+        <Text className="bg-[#E84A28] text-white px-4 py-1 rounded-xl">
+          Anime
+        </Text>
+        <Text className="px-4 py-1">Manhwa</Text>
+        <Text className="px-4 py-1">Manga</Text>
+        <Text className="px-4 py-1">Manhwa</Text>
       </View>
-    </View>
-  )
-}
 
-export default HomeBody
+      <View className="p-2 mt-3">
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {data.map((item, index) => (
+            <View
+              key={index}
+              className="p-4 rounded-3xl bg-white mx-2"
+              style={{ width: 250 }}
+            >
+              <Image
+                source={item.image}
+                className="w-[220] h-[300] rounded-xl"
+              />
+              <View className="flex justify-center items-center mt-3">
+                <Text className="text-lg font-medium">{item.title}</Text>
+                <Text className="text-zinc-400 tracking-tighter">{item.genre}</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+
+    </View>
+  );
+};
+
+export default HomeBody;
--- app/home/homeItems.jsx
+++ app/home/homeItems.jsx
@@ -1,15 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import HomeTop from './homeTop'
-import HomeBody from './homeBody'
+import { View, StyleSheet } from "react-native";
+import React from "react";
+import HomeTop from "./homeTop";
+import HomeBody from "./homeBody";
 
 const Home = () => {
   return (
-    <View className="pt-10">
-      <HomeTop/>
-      <HomeBody/>
+    <View className="pt-10 bg-[#E6E6E6] h-full">
+      <HomeTop />
+      <HomeBody />
     </View>
-  )
-}
-
-export default Home
+  );
+};
+export default Home;
